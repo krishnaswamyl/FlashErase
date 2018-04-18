da3bfa8cf5edfca1ea6d6df395d8f0bbd93ce730
Krishnaswamy Loganathan <krishnaswamy@microlab.co.in>
2018-04-18 14:59:30 +0530

    Add project files.

---
+++ FlashErase/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace FlashErase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonSendSync = new System.Windows.Forms.Button();
+            this.checkBoxLPC1768 = new System.Windows.Forms.CheckBox();
+            this.checkBoxLPC1857 = new System.Windows.Forms.CheckBox();
+            this.buttonErase = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(270, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(181, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Com Port";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(30, 47);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "P2.7";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(30, 70);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(62, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "RESET";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.BackColor = System.Drawing.Color.LimeGreen;
+            this.buttonConnect.Location = new System.Drawing.Point(270, 51);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(121, 23);
+            this.buttonConnect.TabIndex = 3;
+            this.buttonConnect.Text = "CONNECT";
+            this.buttonConnect.UseVisualStyleBackColor = false;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(30, 146);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(268, 205);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // buttonSendSync
+            // 
+            this.buttonSendSync.BackColor = System.Drawing.Color.ForestGreen;
+            this.buttonSendSync.Location = new System.Drawing.Point(30, 93);
+            this.buttonSendSync.Name = "buttonSendSync";
+            this.buttonSendSync.Size = new System.Drawing.Size(148, 23);
+            this.buttonSendSync.TabIndex = 5;
+            this.buttonSendSync.Text = "CONNECT TO TARGET";
+            this.buttonSendSync.UseVisualStyleBackColor = false;
+            this.buttonSendSync.Click += new System.EventHandler(this.buttonSendSync_Click);
+            // 
+            // checkBoxLPC1768
+            // 
+            this.checkBoxLPC1768.AutoSize = true;
+            this.checkBoxLPC1768.Location = new System.Drawing.Point(114, 43);
+            this.checkBoxLPC1768.Name = "checkBoxLPC1768";
+            this.checkBoxLPC1768.Size = new System.Drawing.Size(73, 17);
+            this.checkBoxLPC1768.TabIndex = 6;
+            this.checkBoxLPC1768.Text = "LPC 1768";
+            this.checkBoxLPC1768.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxLPC1857
+            // 
+            this.checkBoxLPC1857.AutoSize = true;
+            this.checkBoxLPC1857.Location = new System.Drawing.Point(114, 66);
+            this.checkBoxLPC1857.Name = "checkBoxLPC1857";
+            this.checkBoxLPC1857.Size = new System.Drawing.Size(73, 17);
+            this.checkBoxLPC1857.TabIndex = 6;
+            this.checkBoxLPC1857.Text = "LPC 1857";
+            this.checkBoxLPC1857.UseVisualStyleBackColor = true;
+            // 
+            // buttonErase
+            // 
+            this.buttonErase.BackColor = System.Drawing.Color.Tomato;
+            this.buttonErase.Location = new System.Drawing.Point(184, 93);
+            this.buttonErase.Name = "buttonErase";
+            this.buttonErase.Size = new System.Drawing.Size(114, 23);
+            this.buttonErase.TabIndex = 5;
+            this.buttonErase.Text = "ERASE FLASH";
+            this.buttonErase.UseVisualStyleBackColor = false;
+            this.buttonErase.Click += new System.EventHandler(this.buttonEraseClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 425);
+            this.Controls.Add(this.checkBoxLPC1857);
+            this.Controls.Add(this.checkBoxLPC1768);
+            this.Controls.Add(this.buttonErase);
+            this.Controls.Add(this.buttonSendSync);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonSendSync;
+        private System.Windows.Forms.CheckBox checkBoxLPC1768;
+        private System.Windows.Forms.CheckBox checkBoxLPC1857;
+        private System.Windows.Forms.Button buttonErase;
+    }
+}
+
---
+++ FlashErase/Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace FlashErase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //show list of valid com ports
+            
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+            }
+            comboBox1.Select();
+            comboBox1.Update();
+            serialPort1 = new SerialPort();
+            buttonErase.Enabled = false;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                serialPort1.RtsEnable = true;
+            }
+            else
+            {
+                serialPort1.RtsEnable = false;
+            }
+        }
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            bool temp;
+            if (checkBox2.Checked)
+            {
+                serialPort1.DtrEnable = true;
+            }
+            else
+            {
+                serialPort1.DtrEnable = false;
+            }
+
+            temp = serialPort1.DtrEnable;
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if(buttonConnect.Text == "CONNECT")
+            {
+                serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                serialPort1.BaudRate = 115200;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Parity = Parity.None;
+                serialPort1.Handshake = Handshake.None;
+                serialPort1.NewLine = "\r\n";
+                serialPort1.WriteTimeout = 1000;
+                serialPort1.ReadTimeout = 3000;
+                if (serialPort1.IsOpen)
+                {
+                    MessageBox.Show("Serial Port alread open");
+                    return;
+                }
+                serialPort1.Open();
+                buttonConnect.Text = "DISCONNECT";
+                buttonConnect.BackColor = Color.Pink;
+
+            }else
+            {
+                serialPort1.Dispose();
+                serialPort1.Close();
+                buttonConnect.Text = "CONNECT";
+                buttonConnect.BackColor = Color.LimeGreen;
+            }
+            
+
+        }
+        private string readComPort()
+        {
+            string res = "NULL";
+            try
+            {
+                res = serialPort1.ReadExisting();      //read response.
+            }
+            catch (TimeoutException e)
+            {
+                MessageBox.Show(e.ToString());
+                
+                return "TimeOut";
+            }
+            return res;
+        }
+
+        private void eraseLPC1857()
+        {
+            string response = "";
+            int delay = 100;
+            serialPort1.DiscardInBuffer();           
+            Thread.Sleep(delay);
+            serialPort1.Write("?");                 //Send "?" to Target.
+            
+            response = readComPort();
+            if(response.Contains("TimeOut"))
+            {
+                MessageBox.Show("Please check wiring and Target connections.");
+            }
+            else
+            {
+                if(response.Contains("Synchronized"))
+                {
+                    richTextBox1.AppendText("Synchronized\r\n");
+                }
+            }
+            Thread.Sleep(delay);
+            serialPort1.Write("Synchronized\r\n");                 //Send "Synchronized\r\n" to Target.
+            response = readComPort();
+            if (response.Contains("TimeOut"))
+            {
+                MessageBox.Show("Please check wiring and Target connections.");
+            }
+            else
+            {
+                if (response.Contains("OK"))
+                {
+                    richTextBox1.AppendText("OK\r\n");
+                }
+            }
+            Thread.Sleep(delay);
+            serialPort1.Write("0\r\n");                 //Send "0\r\n" to Target.
+            response = readComPort();
+            if (response.Contains("TimeOut"))
+            {
+                MessageBox.Show("Please check wiring and Target connections.");
+            }
+            else
+            {
+                if (response.Contains("OK"))
+                {
+                    richTextBox1.AppendText("OK Target ready\r\n");
+                    buttonErase.Enabled = true;
+                }
+            }
+            Thread.Sleep(delay);
+            
+
+
+
+        }
+
+        private void eraseLPC1768()
+        {
+            string treads = "";
+            serialPort1.DiscardInBuffer();
+            richTextBox1.Clear();
+            serialPort1.Write("?");
+            treads = serialPort1.ReadLine();
+            richTextBox1.Text = treads;
+            if (treads.Equals("Synchronized"))
+            {
+                serialPort1.Write("Synchronized\r");
+            }
+            treads = "RESPONSE: " + serialPort1.ReadLine() + "\n";
+            richTextBox1.AppendText(treads);
+            if (treads.Contains("OK"))
+            {
+                serialPort1.Write("12000\r");
+            }
+            treads = "RESPONSE: " + serialPort1.ReadLine() + "\n";
+            richTextBox1.AppendText(treads);
+            if (treads.Contains("OK"))
+            {
+                serialPort1.Write("B 115200 1\r");
+            }
+            treads = "RESPONSE: " + serialPort1.ReadLine() + "\n";
+            richTextBox1.AppendText(treads);
+            if (treads.Contains("0"))
+            {
+                serialPort1.Write("U 23130\r");
+
+            }
+            treads = "RESPONSE: " + serialPort1.ReadLine() + "\n";
+            richTextBox1.AppendText(treads);
+            if (treads.Contains("0"))
+            {
+                serialPort1.Write("P 0 29\r");
+
+            }
+            treads = "RESPONSE: " + serialPort1.ReadLine() + "\n";
+            richTextBox1.AppendText(treads);
+            if (treads.Contains("0"))
+            {
+                serialPort1.Write("E 0 29\r");
+
+            }
+            treads = "RESPONSE: " + serialPort1.ReadLine() + "\n";
+            richTextBox1.AppendText(treads);
+            
+        }
+
+        private void buttonSendSync_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox1.Update();
+            richTextBox1.Text = "Please Wait\r\n";
+            serialPort1.DtrEnable = true;   //Make DTR Signal High. Make RESET pin LOW.
+            serialPort1.RtsEnable = true;   //Make RTS Signal High. Make Pin ISP LOW.
+            Thread.Sleep(500);
+            serialPort1.DtrEnable = false;   //Make DTR Signal Low. Make RESET pin HIGH.
+            Thread.Sleep(600);
+            serialPort1.RtsEnable = false;   //Make RTS Signal Low. Make Pin ISP HIGH.
+            
+            if (serialPort1.IsOpen)
+            {
+                if (checkBoxLPC1857.Checked)
+                {
+                    eraseLPC1857();
+
+                }
+                else
+                    if(checkBoxLPC1768.Checked)
+                {
+                    eraseLPC1768();
+                }
+          
+                
+            }
+            else
+            {
+                MessageBox.Show("Please connect to Comport and try again...");
+            }
+           
+        }
+
+        private void buttonEraseClicked(object sender, EventArgs e)
+        {
+            string response = "";
+            buttonErase.BackColor = Color.OrangeRed;
+            int delay = 250;
+            if(checkBoxLPC1857.Checked)
+            {
+                serialPort1.Write("B 115200 1\r");      //Send ISP to set Baud rate.
+                response = readComPort();
+                if (response.Contains("TimeOut"))
+                {
+                    MessageBox.Show("Please check wiring and Target connections.");
+                }
+                else
+                {
+                    if (response.Contains("0"))
+                    {
+                        richTextBox1.AppendText("Baud Rate Set Success\r\n");
+                    }
+                }
+                Thread.Sleep(delay);
+                serialPort1.Write("U 23130\r");     //send ISP to Unlock Flash.
+                response = readComPort();
+                if (response.Contains("TimeOut"))
+                {
+                    MessageBox.Show("Please check wiring and Target connections.");
+                }
+                else
+                {
+                    if (response.Contains("0"))
+                    {
+                        richTextBox1.AppendText("Unlock Flash success\r\n");
+                    }
+                }
+                Thread.Sleep(delay);
+                serialPort1.Write("P 0 14 0\r");      //send ISP to Prepare flash for Erase Bank A.
+                response = readComPort();
+                if (response.Contains("TimeOut"))
+                {
+                    MessageBox.Show("Please check wiring and Target connections.");
+                }
+                else
+                {
+                    if (response.Contains("0"))
+                    {
+                        richTextBox1.AppendText("Prepare flash for Erase A Success\r\n");
+                    }
+                }
+                Thread.Sleep(delay);
+                serialPort1.Write("P 0 14 1\r");      //send ISP to Prepare flash for Erase Bank B.
+                response = readComPort();
+                if (response.Contains("TimeOut"))
+                {
+                    MessageBox.Show("Please check wiring and Target connections.");
+                }
+                else
+                {
+                    if (response.Contains("0"))
+                    {
+                        richTextBox1.AppendText("Prepare flash for Erase B Success\r\n");
+                    }
+                }
+                Thread.Sleep(delay);
+                serialPort1.Write("E 0 14 0\r");      //send ISP to Erase flash Bank A.
+                response = readComPort();
+                if (response.Contains("TimeOut"))
+                {
+                    MessageBox.Show("Please check wiring and Target connections.");
+                }
+                else
+                {
+                    if (response.Contains("0"))
+                    {
+                        richTextBox1.AppendText("Erase A Success\r\n");
+                    }
+                }
+                Thread.Sleep(delay);
+                serialPort1.Write("E 0 14 1\r");      //send ISP to Erase flash Bank B.
+                response = readComPort();
+                if (response.Contains("TimeOut"))
+                {
+                    MessageBox.Show("Please check wiring and Target connections.");
+                }
+                else
+                {
+                    if (response.Contains("0"))
+                    {
+                        richTextBox1.AppendText("Erase B Success\r\n");
+                    }
+                }
+               // serialPort1.RtsEnable = true;   //Make RTS Signal High. Make Pin ISP LOW.
+                //Thread.Sleep(250);               
+               // serialPort1.RtsEnable = false;   //Make RTS Signal Low. Make Pin ISP HIGH.
+                buttonErase.BackColor = Color.LimeGreen;
+
+            }
+        }
+    }
+}
